8debcec3f499afcec9ca1c454003b1b900bc112a
Amykelani Skhosana <77246887+Amykelani@users.noreply.github.com>
2025-02-14 12:58:45 +0200

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,11 @@
+import pandas as pd
+from .models import get_connection
 
+def export_to_excel():
+    connection = get_connection()
+    query = "SELECT * FROM laptop_log"
+    df = pd.read_sql(query, connection)
+    connection.close()
+    file_path = "Laptop_Log.xlsx"
+    df.to_excel(file_path, index=False)
+    return file_path 
